bbe242cbb84d7b6410ee916a4f8610d856ad9d40
Leo <diegolyt29@gmail.com>
2026-04-05 00:26:46 -0500

    Base

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/index', function(){
+    return response()->json([
+        'mensaje' => 'Base del proyecto listo para enviar el primer commit'
+    ]);
+});
